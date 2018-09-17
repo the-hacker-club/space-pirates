a79c0f2e6e3b0696cae9282f1bd260e970782191
cpadilla <christoferpadilla@gmail.com>
2018-09-16 22:13:21 -0400

    Unbinding the vertex attribute array before we bind it, rather than binding it and then unbind it

--- WarpEngine/ObjectMesh.cpp
+++ WarpEngine/ObjectMesh.cpp
@@ -38,9 +38,8 @@ namespace WarpEngine
 		// TODO: Will need to move this into the shader class
 		// and have shaders specify the vertex attributes
 		// 1. then set the vertex attributes pointers
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
-
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
 		// ====================
 		// Unbind buffers
