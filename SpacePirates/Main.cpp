5fd53ebc9001bfc9e9ee1643ba1ac3bfb2133e16
cpadilla <christoferpadilla@gmail.com>
2018-09-21 14:26:59 -0400

    Changed triangle 2 to rectangle

--- SpacePirates/Main.cpp
+++ SpacePirates/Main.cpp
@@ -33,11 +33,6 @@ int main() {
 		 0.0f,  0.5f, 0.0f
 	};
 
-	vector<int> indicies{
-		0, 1, 3,
-		1, 2, 3
-	};
-
     // Create Vertex Data for triangle 1
 	vector<float> triangle1Verts {
 		// position         	// colors
@@ -63,10 +58,22 @@ int main() {
 		0.5f, 0.5f, 0.0f,
 		1.0f, -0.5f, 0.0f
 	};
+	vector<float> rectangleColorVerts {
+		 1.0f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
+		 1.0f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom right
+		 0.0f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f   // top left 
+	};
+	vector<int> indicies{
+		0, 1, 3,
+		1, 2, 3
+	};
     vector<vAttribute> v2Attribs = vector<vAttribute>();
     index = 0;
-    v2Attribs.push_back(vAttribute(index, 3, WarpEngine::vType::FLOAT, false, 3, 0));
-    VertexData t2VertexData = VertexData(&triangle2Verts, &v2Attribs);
+    v2Attribs.push_back(vAttribute(index, 3, WarpEngine::vType::FLOAT, false, 6, 0));
+    index++;
+    v2Attribs.push_back(vAttribute(index, 3, WarpEngine::vType::FLOAT, false, 6, 3));
+    VertexData t2VertexData = VertexData(&rectangleColorVerts, &indicies, &v2Attribs);
 
     // Create triangle 2
 	unique_ptr<ObjectMesh> triangle2 = make_unique<ObjectMesh>(&t2VertexData);
@@ -78,17 +85,17 @@ int main() {
 	triangle1->addFragmentShader(rainbowFrag.c_str());
 
 	// Load triangle 2 shaders
-	string testVert = glslLoader::load("test.vert");
-	triangle2->addVertexShader(testVert.c_str());
-	string yellowFrag = glslLoader::load("yellow.frag");
-	triangle2->addFragmentShader(yellowFrag.c_str());
+	// string testVert = glslLoader::load("test.vert");
+	triangle2->addVertexShader(rainbowVert.c_str());
+	// string yellowFrag = glslLoader::load("yellow.frag");
+	triangle2->addFragmentShader(rainbowFrag.c_str());
 
 	// Set uniform for triangle 2 shaders
-	unique_ptr<Shader::Uniform4f> ourColor = make_unique<Shader::Uniform4f>("ourColor", 0.0f, 0.0f, 0.0f, 1.0f);
-	triangle2->addShaderUniform(ourColor.get());
+	// unique_ptr<Shader::Uniform4f> ourColor = make_unique<Shader::Uniform4f>("ourColor", 0.0f, 0.0f, 0.0f, 1.0f);
+	// triangle2->addShaderUniform(ourColor.get());
 
 	while (!gameWindow->shouldClose()) {
-		ourColor->y = (sin(getTime()) / 2.0f) + 0.5f;
+		// ourColor->y = (sin(getTime()) / 2.0f) + 0.5f;
 
 		gameWindow->render();
 	}
