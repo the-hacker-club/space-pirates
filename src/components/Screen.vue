926cd4b1874daae3219ab206781400696b0bd41e
cpadilla <christoferpadilla@gmail.com>
2020-02-20 22:23:09 -0500

    Resized Screen to fit canvas. Still needs work, doesn't resize when window changes

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="screen">
-    <h1>Hello</h1>
-  </div>
+  <div id="screen" />
 </template>
 
 <script>
@@ -22,28 +20,51 @@ export default {
     mesh: null
     }
   },
-  created() {
-
-    this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    this.camera.position.z = 1;
-
-    this.scene = new THREE.Scene();
-
-    this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    this.material = new THREE.MeshNormalMaterial();
-
-    this.mesh = new THREE.Mesh( this.geometry, this.material );
-    this.scene.add( this.mesh );
-
-    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( this.renderer.domElement );
-
+  mounted() {
+    this.init();
     this.animate();
   },
   methods: {
-    animate() {
+    init: function() {
+        let container = document.getElementById('screen');
+        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+        this.camera.position.z = 1;
+
+        this.scene = new THREE.Scene();
+
+        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        this.material = new THREE.MeshNormalMaterial();
+
+        this.mesh = new THREE.Mesh( this.geometry, this.material );
+        this.scene.add( this.mesh );
+
+        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this.renderer.setSize( container.clientWidth, container.clientHeight );
+        //this.renderer.setSize(container.width(), container.height());
+        container.appendChild( this.renderer.domElement );
+    },
+    resizeCanvasToDisplaySize: function() {
+      const canvas = this.renderer.domElement;
+      // look up the size the canvas is being displayed
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      console.log("width: " + canvas.width + ", height: " + canvas.height);
+      //console.log("width: " + width + ", height: " + height);
+
+      // adjust displayBuffer size to match
+      if (canvas.width !== width || canvas.height !== height) {
+        // you must pass false here or three.js sadly fights the browser
+        this.renderer.setSize(width, height, false);
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+
+        // update any render target sizes here
+      }
+    },
+    animate: function() {
         requestAnimationFrame( this.animate );
+
+        //this.resizeCanvasToDisplaySize();
 
         this.mesh.rotation.x += 0.01;
         this.mesh.rotation.y += 0.02;
@@ -68,5 +89,9 @@ li {
 }
 a {
   color: #42b983;
+}
+#screen {
+width:100%;
+height:100%;
 }
 </style>
